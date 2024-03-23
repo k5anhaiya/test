70caff1cfd033b4cec5076c9fd6e2aacae2c9f35
unknown <kanhaiya5kumar@gmail.com>
2024-03-23 20:13:52 +0530

    studentTeacher

--- Student.java
+++ Student.java
@@ -3,5 +3,7 @@ public class Student{
     public static void main(String[] args) {
         
         System.out.println("This is Student class.");
+
+        System.out.println("First commit");
     }
 }
--- Teacher.java
+++ Teacher.java
@@ -2,5 +2,7 @@ public class Teacher {
 
     public static void main(String[] args) {
         System.out.println("This is Teacher class.");
+
+        System.out.println("First commit");
     }
 }
